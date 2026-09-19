49039da022eef1fba944448f0c28acba6f0d4a09
TheRealT-Dawg <Johnbabbel2@gmail.com>
2026-09-19 16:42:36 -0400

    Fixed the Parallelization Plotting bug
    Replaced parfor with parsim to bypas transparency violation, called every file into mc_sim_setup and assigned them to the base workforce, and then just made some touchups to the mc_plots

--- sims/hopper_6dof_sizer/load_lookup.m
+++ sims/hopper_6dof_sizer/load_lookup.m
@@ -9,3 +9,29 @@ load('wind_vectors2.mat');
 
 % Load CG MOI Tables
 load("cg_I_LUT.mat");
+
+% --- Load Engine Performance Data ---
+mdot_data = readmatrix("mdot_lookup.xlsx");
+thrust_data = mdot_data(:,1);   % breakpoints
+fu_mdot_data = mdot_data(:,2);
+ox_mdot_data = mdot_data(:,3);
+
+assignin('base', 'thrust_data',  thrust_data);
+assignin('base', 'fu_mdot_data', fu_mdot_data);
+assignin('base', 'ox_mdot_data', ox_mdot_data);
+
+
+% --- Load Wind Data Dynamically ---
+wind_data = load('wind_vectors2.mat');
+wind_fields = fieldnames(wind_data);
+for i = 1:length(wind_fields)
+    assignin('base', wind_fields{i}, wind_data.(wind_fields{i}));
+end
+
+
+% --- Load CG & MOI Tables Dynamically ---
+cg_data = load('cg_I_LUT.mat');
+cg_fields = fieldnames(cg_data);
+for i = 1:length(cg_fields)
+    assignin('base', cg_fields{i}, cg_data.(cg_fields{i}));
+end
